c305bfcca8cbf4803a1d21a9d7a4d063208e5c2d
atmakurmohith <mohith.atmakur@stonybrook.edu>
2025-08-19 17:31:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,3 +83,20 @@ resource "azurerm_virtual_machine" "VM" {
   }
 }
 
+resource "azurerm_storage_account" "storage-account" {
+  name                     = "tt-storage-account"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = "tt-container"
+  storage_account_id    = azurerm_storage_account.rg.id
+  container_access_type = "private"
+}
