04b4a6a4e0157abb2e1d00f962752a0830544d5d
Mohith Atmakur <mohith.atmakur@stonybrook.edu>
2025-08-20 12:42:51 -0400

    added bastion host to create a bastion host, without creating it while connecting vm

--- main.tf
+++ main.tf
@@ -125,3 +125,25 @@ resource "azurerm_windows_virtual_machine" "windowsvm" {
     version   = "latest"
   }
 }
+
+resource "azurerm_public_ip" "bastion_ip" {
+name = "bastion-ip"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+allocation_method = "Static"
+sku = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+name = "bastion-host"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+
+ip_configuration {
+name = "bastion-ip-config"
+subnet_id = azurerm_subnet.bastion.id
+public_ip_address_id = azurerm_public_ip.bastion_ip.id
+}
+
+sku = "Standard"
+}
