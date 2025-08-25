ec4f1efa51dc6fc7ee9deba186e11ba2b6182b0a
Mohith Atmakur <mohith.atmakur@stonybrook.edu>
2025-08-25 08:52:57 -0400

    updated

--- backend.tf
+++ backend.tf
@@ -1,17 +1,17 @@
+# # terraform {
+# #   backend "azurerm" {
+# #     resource_group_name   = "terraformstate.rg"
+# #     storage_account_name  = "devmohithstg123"         
+# #     container_name        = "tfstatecontainer"
+# #     key                   = "${var.environment}-tfstate"
+# #   }
+# # }
+
 # terraform {
 #   backend "azurerm" {
-#     resource_group_name   = "terraformstate.rg"
-#     storage_account_name  = "devmohithstg123"         
-#     container_name        = "tfstatecontainer"
-#     key                   = "${var.environment}-tfstate"
+#     resource_group_name  = "terraformstate.rg"
+#     storage_account_name = "devmohithstg123"
+#     container_name       = "tfstatecontainer"
+#     key                  = ""  # leave this blank; will be set via -backend-config
 #   }
 # }
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraformstate.rg"
-    storage_account_name = "devmohithstg123"
-    container_name       = "tfstatecontainer"
-    key                  = ""  # leave this blank; will be set via -backend-config
-  }
-}
